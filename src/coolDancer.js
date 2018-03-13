b3660ac46d91804d463ca05ad9a87cc033cd01eb
Carey Lee <CareyL96@gmail.com>
2018-03-12 23:52:05 -0700

    blinky dancer and cool dancer working

--- src/coolDancer.js
+++ src/coolDancer.js
@@ -1,5 +1,7 @@
 var makeCoolDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
+  this.$node = $('<span class="coolDancer"></span>');
+  this.setPosition(600, $('body').width() * Math.random());
 };
 
 makeCoolDancer.prototype = Object.create(makeDancer.prototype);
@@ -9,5 +11,16 @@ makeCoolDancer.prototype.constructor = makeCoolDancer;
 var oldStep = makeCoolDancer.prototype.step;
 
 makeCoolDancer.prototype.step = function() {
+  var colors = ['red', 'green', 'blue', 'orange', 'yellow'];
+  var randomHeight = $('body').height() * Math.random();
+  // var randomWidth = $('body').width() * Math.random();
+  var animationTime = 500;
+  $('.coolDancer').animate({top: randomHeight, animationTime});
+  $('.coolDancer').animate({width: this.getRandomInt(50, 100), height: this.getRandomInt(50, 100)}, 200);
+  $('.coolDancer').hover(function() {
+    $(this).height(400);
+    $(this).width(400);
+  });
   oldStep.call(this);
 };
+
--- src/dancer.js
+++ src/dancer.js
@@ -19,3 +19,7 @@ makeDancer.prototype.setPosition = function(top, left) {
 
   this.$node.css(styleSettings);
 };
+
+makeDancer.prototype.getRandomInt = function(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
